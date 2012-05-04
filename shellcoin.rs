61ad4edb3bfc3138b0775011207f0a0e596bd01c
Don Park <don@donpark.org>
2012-05-04 15:48:43 -0700

    more rustness

--- shellcoin.rs
+++ shellcoin.rs
@@ -1,5 +1,19 @@
 // command line utility for shellcoin
+use std;
+use sqlite;
+
+import core::result::extensions;
 
 fn main() {
-  io::println("shellcoin")
+  io::println("shellcoin");
+  let db_result = sqlite::sqlite_open("usercoins.db");
+  if db_result.is_success() {
+    let db = db_result.get();
+    io::println("db open");
+    let exec_result = db.exec("select * from users");
+    if exec_result.is_success() {
+      let row = exec_result.get();
+      io::println(row);
+    }
+  }
 }
